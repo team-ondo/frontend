8def6f21c1896efed98fcbaa9f2d48b37a446ad7
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-08 16:48:15 +0900

    feat: Read css files at app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import "@/styles/reset.scss";
 import "@/styles/globals.scss";
+import "@/styles/responsive.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
