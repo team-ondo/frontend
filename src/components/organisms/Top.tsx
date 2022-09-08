484c47264f21a6d9a9384668f713debb5bb62eee
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-08 21:55:36 +0900

    feat: Create top page

---
+++ src/components/organisms/Top.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "@/styles/components/organisms/Top.module.scss";
+import Weather from "@/components/molecules/Weather";
+
+export default function Top() {
+  return (
+    <div className={styles.top}>
+      <div className={styles.top__inner}>
+       <Weather />
+      </div>
+    </div>
+  );
+}
