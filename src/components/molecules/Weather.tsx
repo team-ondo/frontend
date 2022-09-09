e6a1b5f27038075177f7a07fea7cac04385d888c
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-09 15:51:43 +0900

    feat: Set date on weather component

--- src/components/molecules/Weather.tsx
+++ src/components/molecules/Weather.tsx
@@ -1,42 +1,71 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/styles/components/molecules/Weather.module.scss";
-import axios from "axios";
 
 export default function Weather() {
   const [lat, setLat] = useState<number | null>();
   const [long, setLong] = useState<number | null>();
   const [data, setData] = useState<any>([]);
+  const [day, setDay] = useState<string>("");
 
   const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 
   useEffect(() => {
-    setLat(35.6579702);
-    setLong(139.7276486);
+    const date = new Date();
+    const dayOfWeek = date.getDay();
+    const dayOfWeekStr = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    setDay(`${dayOfWeekStr[dayOfWeek]}, ${month}/${day}/${year}`);
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLat(35.6579702);
+      setLong(139.7276486);
+
+      await fetch(
+        `${WEATHER_URL}?lat=${lat}&lon=${long}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+
     console.log("Latitude is:", lat);
     console.log("Longitude is:", long);
 
-    const url = `${WEATHER_URL}?lat=35.6579702&lon=139.7276486&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`;
-    axios.get(url).then((res) => {
-      setData(res.data);
-    });
   }, [lat, long]);
 
-  if (!data) return null;
-
-  console.log(data);
 
   return (
     <div className={styles.weather}>
-      {typeof data != "undefined" ? (
+      {typeof data.main != 'undefined' ? (
         <>
           <p className={styles.weather__location}>{data.name}</p>
-          <p className={styles.weather__date}>Thursday, 9/8/2022</p>
+          <p className={styles.weather__date}>{day}</p>
           <p className={styles.weather__temperature}>
             {data.main.temp}&#x2103;
           </p>
           <p className={styles.weather__humidity}>{data.main.humidity}%</p>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} width={40} height={40} alt="" />
+          <img
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+            width={40}
+            height={40}
+            alt=""
+          />
         </>
       ) : (
         <div></div>
