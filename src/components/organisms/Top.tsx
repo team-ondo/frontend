12894eb8df37ecb5d7bc104005caa5c43a76e05a
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-12 16:27:33 +0900

    feat: Set current temperature and humidity on Top component

--- src/components/organisms/Top.tsx
+++ src/components/organisms/Top.tsx
@@ -4,19 +4,27 @@ import Weather from "@/components/molecules/Weather";
 import Livedata from "./Livedata";
 
 export default function Top() {
-  const [lat, setLat] = useState<number | null>(0);
-  const [long, setLong] = useState<number | null>(0);
+  const [lat, setLat] = useState<number | null>();
+  const [long, setLong] = useState<number | null>();
+  const [currTemp, setCurrTemp] = useState<number | null>();
+  const [currHumid, setCurrHumid] = useState<number | null>();
 
   useEffect(() => {
     setLat(35.6579702);
     setLong(139.7276486);
-  }, [lat, long]);
+    setCurrTemp(29.2);
+    setCurrHumid(68);
+  }, []);
 
   return (
     <div className={styles.top}>
       <div className={styles.top__inner}>
-        <Weather lat={lat} long={long} />
-        <Livedata />
+        {lat && long ? <Weather lat={lat} long={long} /> : <></>}
+        {currTemp && currHumid ? (
+          <Livedata currTemp={currTemp} currHumid={currHumid} />
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
