ad4b8cf1efa3711496153d5de099d84cb0340303
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-12 10:23:40 +0900

    feat: install chart.js

--- src/components/organisms/Livedata.tsx
+++ src/components/organisms/Livedata.tsx
@@ -1,16 +1,57 @@
 import React from "react";
 import styles from "@/styles/components/organisms/Livedata.module.scss";
+import { ArcElement } from "chart.js";
+import Chart from "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
 
 export default function Livedata() {
+  Chart.register(ArcElement);
+
+  const tempData = {
+    labels: ["Red", "Blue"],
+    datasets: [
+      {
+        label: "Temperature",
+        data: [29.2, 20.8],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  const humidData = {
+    labels: ["Red", "Blue"],
+    datasets: [
+      {
+        label: "Temperature",
+        data: [68, 32],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
   return (
     <div className={styles.livedata}>
       <div className={styles.temperature}>
         <h2>Temperature</h2>
         <div>29.2</div>
+        <div>
+          <Doughnut height={300} width={300} data={tempData} id="temp-chart" />
+        </div>
       </div>
       <div className={styles.humidity}>
         <h2>Humidity</h2>
         <div>68</div>
+        <div>
+          <Doughnut height={300} width={300} data={humidData} id="humid-chart" />
+        </div>
       </div>
     </div>
   );
