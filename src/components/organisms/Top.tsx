51ac76566a7f0a0b6660a020a3a676b18e007e54
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-12 10:23:03 +0900

    feat: change temp data to props

--- src/components/organisms/Top.tsx
+++ src/components/organisms/Top.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "@/styles/components/organisms/Top.module.scss";
 import Weather from "@/components/molecules/Weather";
 import Livedata from "./Livedata";
 
 export default function Top() {
+  const [lat, setLat] = useState<number | null>(0);
+  const [long, setLong] = useState<number | null>(0);
+
+  useEffect(() => {
+    setLat(35.6579702);
+    setLong(139.7276486);
+  }, [lat, long]);
+
   return (
     <div className={styles.top}>
       <div className={styles.top__inner}>
-        <Weather />
+        <Weather lat={lat} long={long}/>
         <Livedata />
       </div>
     </div>
