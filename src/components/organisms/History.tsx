92c67fa284e3fce43cae23d3a1bd7452aeedfa9d
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-14 21:26:46 +0900

    feat: Add test line chart

--- src/components/organisms/History.tsx
+++ src/components/organisms/History.tsx
@@ -1,9 +1,66 @@
 import React from "react";
+// TODO TS check
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+// 'not a function' - chart.js version issue?
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Test",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Temp Test",
+      data: [10, 20, 24, 40, 50, 20, 14],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Humidity Test",
+      data: [20, 10, 34, 20, 55, 40, 24],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
 
 export default function History() {
   return (
-    <div style={{ marginTop: "15px" }}>
-      <h1>History Placeholder</h1>
+    <div style={{ paddingTop: "100px", width: "600px", margin: "0 auto" }}>
+      <Line options={options} data={data} />
     </div>
   );
 }
