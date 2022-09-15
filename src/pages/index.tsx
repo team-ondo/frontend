4d5c1c25fbf5ff133a864c188eebbfef0a7a8cd5
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-15 15:03:34 +0900

    feat: Delete header from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PageTemplate from "@/components/templates/PageTemplate";
-import Header from "@/components/organisms/Header";
 import Top from "@/components/organisms/Top";
 
 export default function Index() {
