56ac63a40cb09aa50b36032cc9f18155106ba85c
jonlld <87318928+jonlld@users.noreply.github.com>
2022-09-16 11:22:09 +0900

    chore: Update name of history page component

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import PageTemplate from "@/components/templates/PageTemplate";
 import History from "@/components/organisms/History";
 
-export default function Index() {
+export default function History() {
   const pageid = "history";
 
   return (
