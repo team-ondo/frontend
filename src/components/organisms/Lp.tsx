f8c9fbdcccef58726065958ad631eba1774bf335
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-16 12:46:31 +0900

    feat: Add background image on LP

--- src/components/organisms/Lp.tsx
+++ src/components/organisms/Lp.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import styles from "@/styles/components/molecules/Lp.module.scss";
+import React, { useState, useEffect } from "react";
+import styles from "@/styles/components/organisms/Lp.module.scss";
+import Form from "@/components/organisms/Form";
 
 export default function Lp() {
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    // get the wiindow width
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    // resize
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <section className={styles.mv}>
+        <div className={styles.mv__bgArea}>
+          <div className={styles.mv__inner}>
+            Welcome!
+            <Form />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
