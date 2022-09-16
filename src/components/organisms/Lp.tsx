b9c73baeb5f37e8fce6150ad2ec48fa1d42ddf56
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-16 17:23:52 +0900

    feat: Create new section of LP

--- src/components/organisms/Lp.tsx
+++ src/components/organisms/Lp.tsx
@@ -32,7 +32,43 @@ export default function Lp() {
                 ensure safety during hotter month.
               </p>
             </div>
-            {width > 750 && <div className={styles.mv__form}><Form /></div>}
+            <div className={styles.mv__form}><Form /></div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.introduction}>
+        <div className={styles.introduction__inner}>
+          <div className={styles.introduction__detail}>
+            <div className={styles.introduction__box}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/icon_graph.png" width={50} height={50} alt="graph" />
+              <div className={styles.introduction__item}>
+                <p className={styles.introduction__title}>
+                  Check Data
+                </p>
+                <p className={styles.introduction__text}>You can see live and historical data.</p>
+              </div>
+            </div>
+            <div className={styles.introduction__box}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/icon_notifi.png" width={50} height={50} alt="graph" />
+              <div className={styles.introduction__item}>
+                <p className={styles.introduction__title}>
+                  Send Notification
+                </p>
+                <p className={styles.introduction__text}>If the home heater, you can get notification.</p>
+              </div>
+            </div>
+            <div className={styles.introduction__box}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/icon_alarm.png" width={50} height={50} alt="graph" />
+              <div className={styles.introduction__item}>
+                <p className={styles.introduction__title}>
+                  Alarm
+                </p>
+                <p className={styles.introduction__text}>You can turn alarm off.</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
