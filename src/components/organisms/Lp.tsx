91bfeec93f9a62f49fcb4ebddf6f38a636db39a9
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-16 14:57:40 +0900

    feat: Update main visual on LP

--- src/components/organisms/Lp.tsx
+++ src/components/organisms/Lp.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/styles/components/organisms/Lp.module.scss";
 import Form from "@/components/organisms/Form";
+import Logo from "@/components/atoms/Logo";
 
 export default function Lp() {
   const [width, setWidth] = useState(0);
@@ -18,14 +19,21 @@ export default function Lp() {
     return () => window.removeEventListener("resize", handleResize);
   });
 
-
   return (
     <>
       <section className={styles.mv}>
         <div className={styles.mv__bgArea}>
           <div className={styles.mv__inner}>
-            Welcome!
-            <Form />
+            <div className={styles.mv__text}>
+              <Logo width={100} height={84} />
+              <p className={styles.mv__textDetail}>
+                Ondo helps people!
+              </p>
+              <p className={styles.mv__textDetailS}>
+                Ondo monitors the temperature and humidity of home in order to ensure safety during hotter month.
+              </p>
+            </div>
+            {width > 750 && <Form />}
           </div>
         </div>
       </section>
