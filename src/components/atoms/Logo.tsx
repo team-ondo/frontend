c1c3cf0efd8fef99eaea7026b747bd9ee7ddc76c
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-16 14:57:00 +0900

    feat: Change the logo take props

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "@/styles/components/atoms/Logo.module.scss";
 
-export default function Logo() {
+type Props = {
+  width: number;
+  height: number;
+};
+
+export default function Logo(props: Props) {
   return (
     <div className={styles.logo}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/logo.png" width={100} height={84} alt="Ondo" />
+      <img src="/logo.png" width={props.width} height={props.height} alt="Ondo" />
       <p className={styles.logo__text}>ONDO</p>
     </div>
   );
