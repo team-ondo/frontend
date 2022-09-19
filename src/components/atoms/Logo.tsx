0058360d426172f7ebbdd1c42d6e70c86cf6a4c7
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-19 12:12:57 +0900

    feat: Change props name of components

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -6,16 +6,11 @@ type Props = {
   height: number;
 };
 
-export default function Logo(props: Props) {
+export default function Logo({ width, height }: Props) {
   return (
     <div className={styles.logo}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/logo.png"
-        width={props.width}
-        height={props.height}
-        alt="Ondo"
-      />
+      <img src="/logo.png" width={width} height={height} alt="Ondo" />
       <p className={styles.logo__text}>ONDO</p>
     </div>
   );
