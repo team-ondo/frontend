e081c053fa16cb8830082c7051287516d4465d9d
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-19 17:10:25 +0900

    feat: Change the weather data from server

--- src/components/molecules/Weather.tsx
+++ src/components/molecules/Weather.tsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/styles/components/molecules/Weather.module.scss";
 import axios from "axios";
-import { SERVER_URL } from "@/config/index";
 
 type Props = {
-  lat: number;
-  long: number;
+  deviceId: number;
 };
 
-export default function Weather(props: Props) {
+export default function Weather({ deviceId }: Props) {
   const [data, setData] = useState<any>([]);
   const [day, setDay] = useState<string>("");
 
-  const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const WEATHER_URL = "http://openweathermap.org/img/wn";
 
   useEffect(() => {
     const date = new Date();
@@ -27,18 +25,18 @@ export default function Weather(props: Props) {
       "Saturday",
     ];
     const months = [
-      "January",
-      "February",
-      "March",
-      "April",
+      "Jan.",
+      "Feb.",
+      "Mar.",
+      "Apr.",
       "May",
       "June",
       "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
+      "Aug.",
+      "Sept.",
+      "Oct.",
+      "Nov.",
+      "Dec.",
     ];
 
     const month = months[date.getMonth()];
@@ -47,38 +45,32 @@ export default function Weather(props: Props) {
     setDay(`${dayOfWeekStr[dayOfWeek]}, ${month} ${day}, ${year}`);
   }, []);
 
-  // `${SERVER_URL}/en/1`
   useEffect(() => {
     axios
-      .get(
-        `${WEATHER_URL}?lat=${props.lat}&lon=${props.long}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`
-      )
+      .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/weather-info/en/${deviceId}`)
       .then((res) => {
         setData(res.data);
       });
-  }, [props.lat, props.long]);
-
-  console.log("Latitude is:", props.lat);
-  console.log("Longitude is:", props.long);
+  }, [deviceId]);
 
   return (
     <div className={styles.weather}>
-      {typeof data.main != "undefined" ? (
+      {typeof data != "undefined" ? (
         <>
           <div className={styles.location__info}>
-            <p className={styles.weather__location}>{data.name}</p>
+            <p className={styles.weather__location}>{data.location_name}</p>
             <p className={styles.weather__date}>{day}</p>
           </div>
           <div className={styles.location__data}>
             <p className={styles.weather__temperature}>
-              {data.main.temp}&#x2103;
+              {data.temperature_c}&#x2103;
             </p>
-            <p className={styles.weather__humidity}>{data.main.humidity}%</p>
+            <p className={styles.weather__humidity}>{data.humidity}%</p>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-              width={40}
-              height={40}
+              src={`${WEATHER_URL}/${data.weather_icon}.png`}
+              width={50}
+              height={50}
               alt=""
             />
           </div>
