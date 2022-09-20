36def5b8a7b43fefb77d662c574a82239d454a71
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-20 13:48:23 +0900

    feat: Change the method of data delivery

--- src/components/organisms/Top.tsx
+++ src/components/organisms/Top.tsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import styles from "@/styles/components/organisms/Top.module.scss";
 import Weather from "@/components/molecules/Weather";
 import Livedata from "./Livedata";
+import axios from "axios";
 
 export default function Top() {
   const [currTemp, setCurrTemp] = useState<number | null>();
   const [currHumid, setCurrHumid] = useState<number | null>();
   const [deviceId, setDeviceId] = useState<number | null>(1);
+  const [data, setData] = useState<any>([]);
 
   useEffect(() => {
     setCurrTemp(35.2);
@@ -14,10 +16,18 @@ export default function Top() {
     setDeviceId(1);
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/weather-info/en/${deviceId}`)
+      .then((res) => {
+        setData(res.data);
+      });
+  }, [deviceId]);
+
   return (
     <div className={styles.top}>
       <div className={styles.top__inner}>
-        {deviceId ? <Weather deviceId={deviceId} /> : <></>}
+        {data ? <Weather data={data} /> : <></>}
         {currTemp && currHumid ? (
           <Livedata currTemp={currTemp} currHumid={currHumid} />
         ) : (
