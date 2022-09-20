029dee4f1c146da4648101b34cb71da348780fff
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-20 11:51:22 +0900

    feat: Read weather data from the server

--- src/components/organisms/Top.tsx
+++ src/components/organisms/Top.tsx
@@ -4,22 +4,20 @@ import Weather from "@/components/molecules/Weather";
 import Livedata from "./Livedata";
 
 export default function Top() {
-  const [lat, setLat] = useState<number | null>();
-  const [long, setLong] = useState<number | null>();
   const [currTemp, setCurrTemp] = useState<number | null>();
   const [currHumid, setCurrHumid] = useState<number | null>();
+  const [deviceId, setDeviceId] = useState<number | null>(1);
 
   useEffect(() => {
-    setLat(35.6579702);
-    setLong(139.7276486);
     setCurrTemp(35.2);
     setCurrHumid(68);
+    setDeviceId(1);
   }, []);
 
   return (
     <div className={styles.top}>
       <div className={styles.top__inner}>
-        {lat && long ? <Weather lat={lat} long={long} /> : <></>}
+        {deviceId ? <Weather deviceId={deviceId} /> : <></>}
         {currTemp && currHumid ? (
           <Livedata currTemp={currTemp} currHumid={currHumid} />
         ) : (
