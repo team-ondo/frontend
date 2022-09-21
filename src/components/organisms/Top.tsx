2b8f87427fd55d50de468169cec1833b0354eba5
Takashi Takeda <cctakashitakeda456@gmail.com>
2022-09-21 11:53:16 +0900

    Revert "feat: Add comment on fetch part"
    This reverts commit 378d17c10b79a7ae16cb8566d246a2918a7a955e.

--- src/components/organisms/Top.tsx
+++ src/components/organisms/Top.tsx
@@ -36,7 +36,6 @@ export default function Top({deviceId}: Props) {
       });
 
     // live data
-    // TODO Implement catch
     axios
     .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/device-data/${deviceId}/live`)
     .then((res) => {
