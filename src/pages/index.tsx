09241e38d9625f6a58e1293727ee2e645d6bd35a
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-21 12:25:58 +0900

    style: Format some components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,13 @@ import PageTemplate from "@/components/templates/PageTemplate";
 import Lp from "@/components/organisms/Lp";
 import Top from "@/components/organisms/Top";
 
-
 export default function Index() {
   const pageid = "index";
   const [isLoggedin, setLoggedin] = useState<boolean>(false);
   const [page, setPage] = useState<any>();
-  const [deviceId, setDeviceId] = useState<string | null>("a7382f5c-3326-4cf8-b717-549affe1c2eb");
+  const [deviceId, setDeviceId] = useState<string | null>(
+    "a7382f5c-3326-4cf8-b717-549affe1c2eb"
+  );
 
   useEffect(() => {
     if (isLoggedin) {
@@ -18,13 +19,13 @@ export default function Index() {
         <PageTemplate pageid={pageid}>
           <Top deviceId={deviceId} />
         </PageTemplate>
-      )
+      );
     } else {
       setPage(
         <LandingPageTemplate pageid={pageid}>
           <Lp setLoggedin={setLoggedin} />
         </LandingPageTemplate>
-      )
+      );
     }
   }, [isLoggedin]);
 
