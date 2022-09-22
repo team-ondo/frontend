194b52f0e6efa3a2303c6198bf9f12b815e26a60
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-09-22 12:10:58 +0900

    fix: Replace axios to api

--- src/lib/axios_settings.ts
+++ src/lib/axios_settings.ts
@@ -1,18 +1,18 @@
 import { create } from "middleware-axios/dist";
 
 const api = create({
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 });
 
 api.use(async (config, next, defaults) => {
-    const token = sessionStorage.getItem("ondo-token");
-    if (token != null) {
-        config.headers = {
-            "Authorization": `Bearer ${token}`
-        };
-    }
+  const token = sessionStorage.getItem("ondo-token");
+  if (token != null) {
+    config.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
 
-    await next(config);
+  await next(config);
 });
 
 export default api;
