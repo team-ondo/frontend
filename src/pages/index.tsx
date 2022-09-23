2e8be654f28cde34009d0db7b3ad38e25c508a30
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-23 21:31:48 +0900

    feat: Set status of isLiggedin when have access token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,29 @@ import PageTemplate from "@/components/templates/PageTemplate";
 import Lp from "@/components/organisms/Lp";
 import Top from "@/components/organisms/Top";
 import api from "@/lib/axios_settings";
+import Cookies from "js-cookie";
 
 export default function Index() {
   const pageid = "index";
-  const [isLoggedin, setLoggedin] = useState<boolean>(false);
+  const [isLoggedin, setLoggedin] = useState<boolean>(
+    Cookies.get("access_token") ? true : false
+  );
   const [page, setPage] = useState<any>();
-  const [deviceId, setDeviceId] = useState<string | null>();
 
   useEffect(() => {
     if (isLoggedin) {
-      api.get('/user/devices').then((res) => {
-        setDeviceId(res.data[0].device_id);
-        setPage(
-          <PageTemplate pageid={pageid}>
-            <Top deviceId={res.data[0].device_id} />
-          </PageTemplate>
-        )
-      }).catch((error: any) => {
-        setLoggedin(false);
-      });
+      api
+        .get("/user/devices")
+        .then((res) => {
+          setPage(
+            <PageTemplate pageid={pageid}>
+              <Top deviceId={res.data[0].device_id} />
+            </PageTemplate>
+          );
+        })
+        .catch((error: any) => {
+          setLoggedin(false);
+        });
     } else {
       setPage(
         <LandingPageTemplate pageid={pageid}>
