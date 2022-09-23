2d4c80d50eed5e8e70fc752ea0e75cd494a8ba94
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-23 21:22:04 +0900

    feat: Fetch deviceid from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,26 @@ import LandingPageTemplate from "@/components/templates/LandingPageTemplate";
 import PageTemplate from "@/components/templates/PageTemplate";
 import Lp from "@/components/organisms/Lp";
 import Top from "@/components/organisms/Top";
+import api from "@/lib/axios_settings";
 
 export default function Index() {
   const pageid = "index";
   const [isLoggedin, setLoggedin] = useState<boolean>(false);
   const [page, setPage] = useState<any>();
-  const [deviceId, setDeviceId] = useState<string | null>(
-    "a7382f5c-3326-4cf8-b717-549affe1c2eb"
-  );
+  const [deviceId, setDeviceId] = useState<string | null>();
 
   useEffect(() => {
     if (isLoggedin) {
-      setDeviceId("a7382f5c-3326-4cf8-b717-549affe1c2eb");
-      setPage(
-        <PageTemplate pageid={pageid}>
-          <Top deviceId={deviceId} />
-        </PageTemplate>
-      );
+      api.get('/user/devices').then((res) => {
+        setDeviceId(res.data[0].device_id);
+        setPage(
+          <PageTemplate pageid={pageid}>
+            <Top deviceId={res.data[0].device_id} />
+          </PageTemplate>
+        )
+      }).catch((error: any) => {
+        setLoggedin(false);
+      });
     } else {
       setPage(
         <LandingPageTemplate pageid={pageid}>
