990e9388e3cf17390d034cb414fd53efeb276506
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-24 21:20:30 +0900

    feat: Add type on notifications

--- src/components/organisms/Notifications.tsx
+++ src/components/organisms/Notifications.tsx
@@ -16,172 +16,203 @@ const ReadState = {
 
 type ReadState = typeof ReadState[keyof typeof ReadState];
 
+const typeAlarm = "Alarm";
+const typeSnooze = "Snooze";
+
 const allNotifiData = [
   {
     id: 1,
-    date: "01/01/22 14:30",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "message",
     is_read: true,
   },
   {
     id: 2,
-    date: "03/01/22 16:30",
+    date: "2022-07-01 12:23:45",
+    content_type: "Snooze",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 3,
-    date: "05/01/22 08:01",
+    date: "2022-07-01 12:23:45",
+    content_type: "Out",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 4,
-    date: "01/02/22 19:03",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: true,
   },
   {
     id: 5,
-    date: "06/02/22 12:21",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 6,
-    date: "10/03/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 7,
-    date: "12/04/22 09:10",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 8,
-    date: "15/05/22 11:20",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 9,
-    date: "01/07/22 15:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 10,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 11,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 12,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 13,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 14,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 15,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 16,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 17,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 18,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 19,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 20,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 21,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 22,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 23,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 24,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 25,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 26,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 27,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
   {
     id: 28,
-    date: "01/01/22 14:22",
+    date: "2022-07-01 12:23:45",
+    content_type: "Alarm",
     content: "messagemessagemessagemessagemessage",
     is_read: false,
   },
@@ -255,7 +286,10 @@ export default function Notifications() {
                   key={data.id}
                   onClick={() => handleClickMessage(data)}
                 >
-                  <p className={styles.notifi__itemDate}>{data.date}</p>
+                  <div className={styles.notifi__itemInfo}>
+                    <div className={`${styles.notifi__itemType} ${ data.content_type === typeAlarm ? styles["notifi__itemType--alarm"] : data.content_type === typeSnooze ? styles["notifi__itemType--snooze"] : styles["notifi__itemType--out"]}`}>{data.content_type}</div>
+                    <p className={styles.notifi__itemDate}>{data.date}</p>
+                  </div>
                   <p
                     className={`${styles.notifi__itemContent} ${
                       !data.is_read ? styles.notifi__itemRead : ""
