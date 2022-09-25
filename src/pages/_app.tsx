b48ea99a9aebcf241a621c5b7474d3cd3b29db61
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-25 18:23:49 +0900

    feat: Set recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,14 @@ import "@/styles/reset.scss";
 import "@/styles/globals.scss";
 import "@/styles/responsive.scss";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
