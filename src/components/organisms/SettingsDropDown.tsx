2e8cc5badcf9fc442fb8ef15b7d495828771f333
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-26 18:55:38 +0900

    feat: Remove unnecessary script

--- src/components/organisms/SettingsDropDown.tsx
+++ src/components/organisms/SettingsDropDown.tsx
@@ -1,103 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "@/styles/components/organisms/Settings.module.scss";
 import { styled } from "@stitches/react";
-import { indigo, mauve, blackA } from "@radix-ui/colors";
-import * as TabsPrimitive from "@radix-ui/react-tabs";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
-
-const StyledTabs = styled(TabsPrimitive.Root, {
-  display: "flex",
-  flexDirection: "column",
-  width: 350,
-  boxShadow: `0 2px 10px ${blackA.blackA4}`,
-  borderRadius: 4,
-});
-
-const StyledList = styled(TabsPrimitive.List, {
-  flexShrink: 0,
-  display: "flex",
-  borderBottom: `1px solid ${mauve.mauve6}`,
-});
-
-const StyledTrigger = styled(TabsPrimitive.Trigger, {
-  all: "unset",
-  fontFamily: "inherit",
-  backgroundColor: "white",
-  padding: "0 20px",
-  height: 45,
-  flex: 1,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  fontSize: 15,
-  lineHeight: 1,
-  color: mauve.mauve11,
-  userSelect: "none",
-  "&:first-child": { borderTopLeftRadius: 6 },
-  "&:last-child": { borderTopRightRadius: 6 },
-  "&:hover": { color: indigo.indigo11 },
-  '&[data-state="active"]': {
-    color: indigo.indigo11,
-    boxShadow: "inset 0 -1px 0 0 currentColor, 0 1px 0 0 currentColor",
-    backgroundColor: indigo.indigo3,
-  },
-  "&:focus": { position: "relative" },
-});
-
-const StyledContent = styled(TabsPrimitive.Content, {
-  flexGrow: 1,
-  padding: 20,
-  backgroundColor: "white",
-  borderBottomLeftRadius: 6,
-  borderBottomRightRadius: 6,
-  outline: "none",
-});
-
-const StyledCheckbox = styled(CheckboxPrimitive.Root, {
-  all: "unset",
-  backgroundColor: "white",
-  width: 25,
-  height: 25,
-  borderRadius: 4,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  boxShadow: `0 2px 10px ${blackA.blackA7}`,
-  "&:hover": { backgroundColor: indigo.indigo3 },
-});
-
-const StyledIndicator = styled(CheckboxPrimitive.Indicator, {
-  color: indigo.indigo11,
-});
-
-// Exports
-export const Tabs = StyledTabs;
-export const TabsList = StyledList;
-export const TabsTrigger = StyledTrigger;
-export const TabsContent = StyledContent;
-export const Checkbox = StyledCheckbox;
-export const CheckboxIndicator = StyledIndicator;
-
-const Box = styled("div", {
-  display: "flex",
-  justifyContent: "center",
-});
-const Flex = styled("div", { display: "flex" });
-
-const Text = styled("div", {
-  marginBottom: 20,
-  color: mauve.mauve11,
-  fontSize: 15,
-  lineHeight: 1.5,
-});
-
-const Annotation = styled("p", {
-  marginTop: 5,
-  color: mauve.mauve11,
-  fontSize: 12,
-  lineHeight: 1.5,
-});
+import { indigo } from "@radix-ui/colors";
 
 const Button = styled("button", {
   all: "unset",
@@ -126,49 +30,6 @@ const Button = styled("button", {
   defaultVariants: {
     variant: "indigo",
   },
-});
-const Fieldset = styled("fieldset", {
-  all: "unset",
-  marginBottom: 15,
-  width: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "flex-start",
-});
-
-const Label = styled("label", {
-  fontSize: 13,
-  lineHeight: 1,
-  marginBottom: 10,
-  color: indigo.indigo12,
-  display: "block",
-});
-
-const Input = styled("input", {
-  all: "unset",
-  flex: "1 0 auto",
-  borderRadius: 4,
-  padding: "0 10px",
-  fontSize: 15,
-  lineHeight: 1,
-  color: indigo.indigo11,
-  boxShadow: `0 0 0 1px ${indigo.indigo7}`,
-  height: 35,
-  "&:focus": { boxShadow: `0 0 0 2px ${indigo.indigo8}` },
-  "&::placeholder": { color: mauve.mauve7 },
-});
-
-const FieldCheck = styled("fieldset", {
-  all: "unset",
-  width: "100%",
-  display: "flex",
-  alignItems: "center",
-});
-
-const CheckLink = styled("a", {
-  color: indigo.indigo11,
-  textDecoration: "underline",
-  "&:visited": { color: indigo.indigo11 },
 });
 
 type Props = {
@@ -216,23 +77,28 @@ export default function DropDownSettings({
         <div className={styles.device}>
           <div className={styles.device__inner}>
             <div className={styles.top__device}>
-            <div className={styles.select_device}>
-              <h3 className={styles.select_device__heading}>Please select your device:</h3>
-              <select className={styles.select_device__menu} id="drop_menu_settings">
-                {deviceData.map((obj: any, i: number) => {
-                  return (
-                    <option key={i} value={obj.device_name}>
-                      {obj.device_name}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className={styles.select_device__button}>
-            <Button title={"Select"} onClick={() => settingsViewChange()}>
-              Select
-            </Button>
-            </div>
+              <div className={styles.select_device}>
+                <h3 className={styles.select_device__heading}>
+                  Please select your device:
+                </h3>
+                <select
+                  className={styles.select_device__menu}
+                  id="drop_menu_settings"
+                >
+                  {deviceData.map((obj: any, i: number) => {
+                    return (
+                      <option key={i} value={obj.device_name}>
+                        {obj.device_name}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <div className={styles.select_device__button}>
+                <Button title={"Select"} onClick={() => settingsViewChange()}>
+                  Select
+                </Button>
+              </div>
             </div>
           </div>
         </div>
